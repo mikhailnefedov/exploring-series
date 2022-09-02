a70da1da3c4e1cf441e308b3cef9473dad4f2e7e
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2022-09-02 14:00:11 +0200

    First configuration of redis connection

--- exploring-redis/src/main/java/exploring/redis/RedisController.java
+++ exploring-redis/src/main/java/exploring/redis/RedisController.java
@@ -1,5 +1,6 @@
 package exploring.redis;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(name = "Redis", produces = "application/json", path = "/redis")
 public class RedisController {
 
+    private final RedisTemplate<String, Object> template;
+
+    public RedisController(RedisTemplate<String, Object> template) {
+        this.template = template;
+    }
 
     @GetMapping()
     public ResponseEntity getTest() {
-        return ResponseEntity.ok("test");
+        Object result = template.opsForValue().get("key:1");
+        return ResponseEntity.ok(result);
     }
 }
